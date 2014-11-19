d18ce9dc6dce09ab22ff23baf6bd802523e49713
borygo <wojciechszopinski@wp.pl>
2014-11-19 21:15:23 +0100

    test commit, deploy bat

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+echo "Copying started"
+copy *.html E:\Programy\glassfish-4.0\glassfish\domains\domain1\docroot
+copy css\* E:\Programy\glassfish-4.0\glassfish\domains\domain1\docroot\css
+copy js\* E:\Programy\glassfish-4.0\glassfish\domains\domain1\docroot\js
+echo "Copying finished"
+pause
